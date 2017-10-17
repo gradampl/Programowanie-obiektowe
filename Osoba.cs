ff293d668728a5d3c54c175dcbb1f72602d06b32
gradampl <gr_adam@o2.pl>
2017-10-17 22:03:27 +0200

    Add files via upload

---
+++ Osoba.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Osoba
+    {
+        public string imie;
+        public string nazwisko;
+        public int rokUrodzenia;
+        public int waga;
+        public int wzrost;
+        public bool okulary;
+        public string plec;
+
+        public int ObliczWiek()
+        {
+            return (DateTime.Now.Year - rokUrodzenia);
+        }
+        public void DodajPrzedrostek()
+        {
+            if (plec == "m")
+                Console.WriteLine("{0} {1} jest mezczyzna", imie, nazwisko);
+            else if (plec == "k")
+                Console.WriteLine("{0} {1} jest kobieta", imie, nazwisko);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Osoba dyrektor = new Osoba();
+            dyrektor.imie = "Wojciech";
+            dyrektor.nazwisko = "Jaśkiewicz";
+            dyrektor.rokUrodzenia = 1950;
+            dyrektor.plec = "m";
+
+           // Console.WriteLine("{0} {1}, wiek lat {2}.",dyrektor.imie,dyrektor.nazwisko,dyrektor.ObliczWiek());
+            dyrektor.DodajPrzedrostek();
+            
+            Console.ReadKey();
+        }
+    }
+}
